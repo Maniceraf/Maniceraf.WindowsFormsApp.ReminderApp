04a4ab07e2223adfe2721250a9c9c1a48ba7fb4c
iamloser <hungnguyen8102000@gmail.com>
2023-12-30 16:15:01 +0700

    Add project files.

---
+++ ReminderApp/Main.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ReminderApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.reminder = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.hideButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(9, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Setup Time:";
+            // 
+            // comboBox
+            // 
+            this.comboBox.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "AM",
+            "PM"});
+            this.comboBox.Location = new System.Drawing.Point(200, 29);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(196, 25);
+            this.comboBox.TabIndex = 2;
+            this.comboBox.SelectedIndex = 0;
+            // 
+            // reminder
+            // 
+            this.reminder.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.reminder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.reminder.Location = new System.Drawing.Point(100, 60);
+            this.reminder.Name = "reminder";
+            this.reminder.Size = new System.Drawing.Size(296, 149);
+            this.reminder.TabIndex = 3;
+            this.reminder.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(9, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Reminder:";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(100, 215);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(150, 30);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.Red;
+            this.stopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.stopButton.ForeColor = System.Drawing.Color.White;
+            this.stopButton.Location = new System.Drawing.Point(256, 215);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(140, 30);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // maskedTextBox
+            // 
+            this.maskedTextBox.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.maskedTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.maskedTextBox.Location = new System.Drawing.Point(100, 29);
+            this.maskedTextBox.Mask = "00:00:00";
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(94, 25);
+            this.maskedTextBox.TabIndex = 7;
+            this.maskedTextBox.Text = "000000";
+            this.maskedTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox.ValidatingType = typeof(System.DateTime);
+            // 
+            // hideButton
+            // 
+            this.hideButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.hideButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hideButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.hideButton.ForeColor = System.Drawing.Color.White;
+            this.hideButton.Location = new System.Drawing.Point(100, 251);
+            this.hideButton.Name = "hideButton";
+            this.hideButton.Size = new System.Drawing.Size(150, 30);
+            this.hideButton.TabIndex = 8;
+            this.hideButton.Text = "HIDE";
+            this.hideButton.UseVisualStyleBackColor = false;
+            this.hideButton.Click += new System.EventHandler(this.hideButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelCurrentTime);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 50);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Current Time:";
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.AutoSize = true;
+            this.labelCurrentTime.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelCurrentTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelCurrentTime.Location = new System.Drawing.Point(9, 21);
+            this.labelCurrentTime.Name = "labelCurrentTime";
+            this.labelCurrentTime.Size = new System.Drawing.Size(74, 17);
+            this.labelCurrentTime.TabIndex = 0;
+            this.labelCurrentTime.Text = "00:00:00 00";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.refreshButton);
+            this.groupBox2.Controls.Add(this.reminder);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.hideButton);
+            this.groupBox2.Controls.Add(this.comboBox);
+            this.groupBox2.Controls.Add(this.maskedTextBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.stopButton);
+            this.groupBox2.Controls.Add(this.startButton);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(12, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(410, 298);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Setting Remind:";
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.refreshButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.refreshButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.refreshButton.ForeColor = System.Drawing.Color.White;
+            this.refreshButton.Location = new System.Drawing.Point(256, 251);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(140, 30);
+            this.refreshButton.TabIndex = 9;
+            this.refreshButton.Text = "REFRESH";
+            this.refreshButton.UseVisualStyleBackColor = false;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelResult);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox3.Location = new System.Drawing.Point(12, 372);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(410, 50);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelResult.Location = new System.Drawing.Point(9, 22);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(40, 17);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Text = "None";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon.BalloonTipTitle = "Reminder App is minimized";
+            this.notifyIcon.Text = "Let do it !!!";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(434, 440);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reminder Application";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox;
+        private RichTextBox reminder;
+        private Label label2;
+        private Button startButton;
+        private Button stopButton;
+        private MaskedTextBox maskedTextBox;
+        private Button hideButton;
+        private GroupBox groupBox1;
+        private Label labelCurrentTime;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Label labelResult;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private NotifyIcon notifyIcon;
+        private Button refreshButton;
+    }
+}
---
+++ ReminderApp/Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReminderApp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        string currentTime = string.Empty;
+        string messageTime = string.Empty;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentTime = DateTime.Now.ToString("hh:mm:ss tt");
+            labelCurrentTime.Text = currentTime;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            messageTime = maskedTextBox.Text + " " + comboBox.Text;
+            labelResult.Text = "Set reminders at: " + messageTime;
+            if(currentTime == messageTime)
+            {
+                timer2.Stop();
+                MessageBox.Show("The \"Reminder Application\" app wants to remind you that it's time: " + reminder.Text, "Reminder Application", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                startButton.Enabled = true;
+                stopButton.Enabled = false;
+                labelResult.Text = "None";
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string note = reminder.Text;
+            if (!string.IsNullOrEmpty(note))
+            {
+                timer2.Start();
+                startButton.Enabled = false;
+                stopButton.Enabled = true;
+            }
+            else
+            {
+                startButton.Enabled = false;
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            labelResult.Text = "None";
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = true;
+            notifyIcon.BalloonTipText = "Reminder Application";
+            notifyIcon.BalloonTipTitle = "Welcome back !!!";
+            notifyIcon.Icon = SystemIcons.Application;
+            this.Hide();
+            notifyIcon.ShowBalloonTip(5000);
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon.Visible = false;
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            maskedTextBox.Text = "00:00:00";
+            comboBox.SelectedIndex = 0;
+            labelResult.Text = "None";
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            reminder.Text = "";
+        }
+    }
+}
